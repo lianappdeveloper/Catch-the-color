4959a13e66bdf72999cbaaeda6c70f8b2b0c3cb1
lianappdeveloper <45312231+lianappdeveloper@users.noreply.github.com>
2020-07-26 10:12:33 +0300

    Add files via upload

---
+++ Catch the color game/index.js
@@ -0,0 +1,22 @@
+
+
+// let level,lives;
+
+
+// level = 5;
+// let gameFlow = ()=>{ 
+// for (let i = 0; i< level; i++) {
+//     //change the element focus and color to yellow according to the index of it
+//     //if its not colored put it on not focused with CSS
+//     //If you pick the right color it becomes green if not red
+//     let div = document.createElement("div"); //create new div
+//     div.addEventListener('click',gameFlow)
+//     div.innerHTML = level;
+//   //  document.getElementById("div").appendChild(div);
+//     console.log(div)
+//     console.log(level);
+//     console.log(i)
+    
+//   }
+//  }
+//  gameFlow();
